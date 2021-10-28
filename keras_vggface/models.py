b37759edc0b6acdbbe35a7dee23501720650b479
Krish Ravindranath <krish.ravindranath@gmail.com>
2021-10-28 16:01:14 -0400

    update deprecated keras calls

--- keras_vggface/models.py
+++ keras_vggface/models.py
@@ -12,12 +12,14 @@
 from keras.layers import Flatten, Dense, Input, GlobalAveragePooling2D, \
     GlobalMaxPooling2D, Activation, Conv2D, MaxPooling2D, BatchNormalization, \
     AveragePooling2D, Reshape, Permute, multiply
-from keras_applications.imagenet_utils import _obtain_input_shape
+from keras.applications.imagenet_utils import obtain_input_shape as _obtain_input_shape
+# from keras_applications.imagenet_utils import _obtain_input_shape
 from keras.utils import layer_utils
 from keras.utils.data_utils import get_file
 from keras import backend as K
 from keras_vggface import utils
-from keras.engine.topology import get_source_inputs
+# from keras.engine.topology import get_source_inputs
+from keras.utils.layer_utils import get_source_inputs
 import warnings
 from keras.models import Model
 from keras import layers
