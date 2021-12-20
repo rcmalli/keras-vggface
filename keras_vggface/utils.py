97a512377f9ead550a74595b657c7ec9b8161e75
ma7555 <ma7moud_alaa@hotmail.com>
2021-12-20 23:53:19 +0200

    fix more imports in utils

--- keras_vggface/utils.py
+++ keras_vggface/utils.py
@@ -9,7 +9,7 @@
 
 import numpy as np
 from tensorflow.keras import backend as K
-from tensorflow.keras.utils.data_utils import get_file
+from tensorflow.keras.utils import get_file
 
 V1_LABELS_PATH = 'https://github.com/rcmalli/keras-vggface/releases/download/v2.0/rcmalli_vggface_labels_v1.npy'
 V2_LABELS_PATH = 'https://github.com/rcmalli/keras-vggface/releases/download/v2.0/rcmalli_vggface_labels_v2.npy'
